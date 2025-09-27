86ff4c3501e3c5b81b6b0c038577337600cbdd98
Ajay Jakate <ajay.jakate@gmail.com>
2025-09-27 01:53:57 -0500

    something like a dropdown working

--- src/metrajay/app.cljs
+++ src/metrajay/app.cljs
@@ -2,6 +2,7 @@
   (:require [reagent.core :as r]
             [metrajay.events :as events]
             [reagent.dom.client :as rdomc]
+            [clojure.string :as str]
             [re-frame.core :as rf]))
 
 
@@ -15,16 +16,70 @@
    [:br]
    [:button.nes-btn.is-warning {:on-click #(rf/dispatch [:download-schedule])} "download schedule"]])
 
+(defn search-component []
+  [:div.flex-flex-col.mx-3
+   [:div.nes-field
+    [:label "Select first station"]
+    [:input.nes-input {:type "text" :list "stations"}]
+    [:datalist {:id "stations"}
+     [:option "Station 1"]
+     [:option "Station 2"]]]
+   [:div.nes-field
+    [:label "Select second station"]
+    [:input.nes-input {:type "text" :disabled true}]]])
+
+(defn searchable-dropdown []
+  (let [query (r/atom "")
+        open? (r/atom false)
+        options ["Union Station" "Ridgewood" "Oak Park" "Downers Grove" "Halstead" "Geneva"]]
+    (fn []
+      [:div.nes-container.with-title.is-centered.mt-3.mx-3
+       [:p.title "Search Route"]
+
+       ;; Input
+       [:p "First Station"]
+       [:input.nes-input.w-full
+        {:type "text"
+         :placeholder "Search..."
+         :value @query
+         :on-focus #(reset! open? true)
+         :on-change #(reset! query (.. % -target -value))}]
+
+       ;; Dropdown
+       (when @open?
+         [:div#dropdown.nes-list.is-disc.bg-white.max-h-100.overflow-y-auto.mt-2.border.border-gray-300
+          (for [opt (filter #(str/includes?
+                              (.toLowerCase %)
+                              (.toLowerCase @query))
+                            options)]
+            ^{:key opt}
+            [:button.nes-btn.w-full.text-left
+             {:on-click (fn []
+                          (reset! query opt)
+                          (reset! open? false))}
+             opt])])
+       
+
+       [:p "more text"]
+       [:p "more text"]
+       [:p "more text"]
+       [:p "more text"]
+       [:p "more text"]
+       
+       
+       
+       ])))
+
 (defn header []
-  [:div.bg-red-500.flex-col.justify-between
-   [:h1 "metrajay"]
-   [:div.flex.flex-row-reverse
-    [:p "last updated: "]]])
+  [:div.bg-red-500.flex.justify-between
+   [:p.ml-2 "metrajay"]
+   [:div
+    [:p.mr-2 "updated"]]])
 
 (defn root-component []
   [:div.flex.flex-col.w-full.max-w-lg.mx-auto
    [header]
-   [simple-component]])
+   [searchable-dropdown]])
 
 (defonce root (delay (rdomc/create-root (.getElementById js/document "root"))))
 
--- src/metrajay/events.cljs
+++ src/metrajay/events.cljs
@@ -8,11 +8,26 @@
    [akiroz.re-frame.storage :refer [persist-db-keys]]))
 
 
+(def sub-states
+  [:page
+   :available-stations
+   ])
+
+(doseq [event sub-states]
+  (rf/reg-event-db
+   event
+   (fn [db [_ val]]
+     (assoc db event val)))
+  (rf/reg-sub
+   event
+   (fn [db _]
+     (-> db event))))
+
 (defn persisted-reg-event-db
   [event-id handler]
   (rf/reg-event-fx
    event-id
-   [(persist-db-keys :metrajay-app [:last_updated :db_version])]
+   [(persist-db-keys :metrajay-app [:update-string :db-version :last-updated])]
    (fn [{:keys [db]} event-vec]
      {:db (handler db event-vec)})))
 
@@ -21,10 +36,12 @@
 (rf/reg-event-fx
  :init-db
  (fn [{:keys [db]} [_ _]]
-   (let [raw-version (:db_version db)
+   (let [raw-version (:db-version db)
          version (if (nil? raw-version) 0 raw-version)
          reset-commands (appdb/db-reset-commands version)
-         full-commands (conj reset-commands #(rf/dispatch [:update-db-version version]))]
+         full-commands (-> reset-commands
+                           (conj #(rf/dispatch [:update-db-version version]))
+                           (conj #(rf/dispatch [:check-for-update])))]
      (appdb/run-commands full-commands)
      {})))
 
@@ -37,7 +54,7 @@
             :mode :cors
             :credentials :omit
             :response-content-types {#"text/plain" :text}
-            :on-success [:set-update]
+            :on-success [:check-update-value]
             :on-failure [:bad-fetch-result]}}))
 
 (rf/reg-event-fx
@@ -48,12 +65,23 @@
             :mode :cors
             :credentials :omit
             :response-content-types {#"application/zip" :array-buffer}
-            :on-success [:zip-success]}}))
+            :on-success [:zip-success]
+            :on-failure [:bad-fetch-result]}}))
 
-(rf/reg-event-db
- :set-update
- (fn [db [_ {:keys [body]}]]
-   (assoc db :update body)))
+(rf/reg-event-fx
+ :bad-fetch-result
+ (fn [{:keys [db]} [_ response]]
+   (js/console.log "Bad fetch result: " (:body response))
+   (js/console.log "Full response obj: " response)))
+
+(rf/reg-event-fx
+ :check-update-value
+ (fn [{:keys [db]} [_ response]]
+   (let [body (:body response)
+         current-string (:update-string db)]
+     (if (= body current-string)
+       {:dispatch [:update-last-updated]}
+       {:fx [[:disptach [:update-update-string body]] [:dispatch [:download-schedule]]]}))))
 
 (rf/reg-event-db
  :set-debug
@@ -79,13 +107,13 @@
 (rf/reg-event-fx
  :increment-db-and-load-csvs
  (fn [{:keys [db]} [_ table-csvs]]
-   (let [new-db-version (inc (:db_version db))
+   (let [new-db-version (inc (:db-version db))
          commands (appdb/db-reset-commands new-db-version)
          full-commands (-> commands
                            (conj #(rf/dispatch [:update-db-version new-db-version]))
                            (conj #(rf/dispatch [:load-new-db table-csvs])))]
      (appdb/run-commands full-commands)
-     {:db (assoc db :db_version new-db-version :debug table-csvs)})))
+     {})))
 
 
 (defn map-entry-to-command [table-csv]
@@ -101,6 +129,7 @@
    (let [commands (mapv map-entry-to-command table-csvs)
          full-commands (-> commands
                            (conj #(js/console.log "All tables loaded and persisted to IndexedDB."))
+                           (conj #(rf/dispatch [:update-last-updated]))
                            (conj #(rf/dispatch [:remove-old-dbs])))]
      (appdb/run-commands full-commands)
      {})))
@@ -113,7 +142,7 @@
        (.then (fn [dbs]
                 (doseq [odb dbs]
                   (let [db-name (.-name odb)
-                        current-name (str "metrajay_v" (:db_version db))]
+                        current-name (str "metrajay_v" (:db-version db))]
                     (when (and
                            (re-find #"metrajay_v[0-9]+$" db-name)
                            (not= db-name current-name))
@@ -129,4 +158,15 @@
 (persisted-reg-event-db
  :update-db-version
  (fn [db [_ version]]
-   (assoc db :db_version version)))
+   (assoc db :db-version version)))
+
+(persisted-reg-event-db
+ :update-last-updated
+ (fn [db [_ _]]
+   (assoc db :last-updated (js/Date.))))
+
+(persisted-reg-event-db
+ :update-update-string
+ (fn [db [_ val]]
+   (assoc db :update-string val)))
+
