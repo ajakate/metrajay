2d50080c474be5f2b37c7bd2bc7f9674d0d30360
Ajay Jakate <ajay.jakate@gmail.com>
2025-09-26 22:49:14 -0500

    simple test on css; release

--- src/metrajay/app.cljs
+++ src/metrajay/app.cljs
@@ -4,19 +4,29 @@
             [reagent.dom.client :as rdomc]
             [re-frame.core :as rf]))
 
+
 (defn simple-component []
   [:div
    [:p.text-5xl "I am nothing"]
    [:p.someclass
     "I have " [:strong "bold"]
     [:span {:style {:color "red"}} " and red "] "text."]
-   [:button {:on-click #(rf/dispatch [:check-for-update])} "update"]
+   [:button.nes-btn.is-primary {:type "button" :on-click #(rf/dispatch [:check-for-update])} "update"]
    [:br]
-   [:button {:on-click #(rf/dispatch [:download-schedule])} "download schedule"]])
+   [:button.nes-btn.is-warning {:on-click #(rf/dispatch [:download-schedule])} "download schedule"]])
+
+(defn header []
+  [:div.bg-red-500
+   [:h1 "metrajay"]])
+
+(defn root-component []
+  [:div.flex.flex-col.w-full.max-w-lg.mx-auto
+   [header]
+   [simple-component]])
 
 (defonce root (delay (rdomc/create-root (.getElementById js/document "root"))))
 
 (defn ^:export ^:dev/after-load init []
   (rf/dispatch-sync [:init-local-storage])
   (rf/dispatch [:init-db])
-  (rdomc/render @root [simple-component]))
+  (rdomc/render @root [root-component]))
