7e915c1ba77e217afe574693a5a29c0e664c1989
Ajay Jakate <ajay.jakate@gmail.com>
2025-10-01 21:32:14 -0500

    full flow basic seems to work

--- src/metrajay/app.cljs
+++ src/metrajay/app.cljs
@@ -5,17 +5,9 @@
             [reagent.dom.client :as rdomc]
             [metrajay.util :as u]
             [clojure.string :as str]
+            [reitit.frontend :as reitit]
+            [reitit.frontend.easy :as rfe]
             [re-frame.core :as rf]))
-
-(defn simple-component []
-  [:div
-   [:p.text-5xl "I am nothing"]
-   [:p.someclass
-    "I have " [:strong "bold"]
-    [:span {:style {:color "red"}} " and red "] "text."]
-   [:button.nes-btn.is-primary {:type "button" :on-click #(rf/dispatch [:check-for-update])} "update"]
-   [:br]
-   [:button.nes-btn.is-warning {:on-click #(rf/dispatch [:download-schedule])} "download schedule"]])
 
 (defn query-explorer []
   (let [query (r/atom "select * from stops limit 10")] ; start with default SQL
@@ -51,21 +43,23 @@
                    ^{:key col}
                    [:td (str (get row col))])])]])]]))))
 
-(defn searchable-dropdown [subscription-keyword title set-station-event]
-  (let [query (r/atom "")
-        open? (r/atom false)]
+(defn searchable-dropdown [subscription-keyword title set-station-event station-sub query-event query-sub]
+  (let [open? (r/atom false)]
     (fn []
       (let [available-stations (rf/subscribe [subscription-keyword])
+            selected-station (rf/subscribe [station-sub])
+            query (rf/subscribe [query-sub])
             options @available-stations]
-        [:div.mt-3        
+        [:div.mt-3
          [:p title]
          [:input.nes-input.w-full
           {:type "text"
            :placeholder "Search..."
-           :value @query
+           :value (if @selected-station (:stop_name @selected-station) @query)
            :on-focus #(reset! open? true)
-           :on-change #(reset! query (.. % -target -value))}]
-         
+           :on-change #(rf/dispatch [query-event (.. % -target -value)])
+           :disabled (not= @selected-station nil)}]
+
          ;; Dropdown
          (when @open?
            [:div#dropdown.nes-list.is-disc.bg-white.max-h-100.overflow-y-auto.mt-2.border.border-gray-300
@@ -76,7 +70,6 @@
               ^{:key opt}
               [:button.nes-btn.w-full.text-left
                {:on-click (fn []
-                            (reset! query opt)
                             (reset! open? false)
                             (rf/dispatch [set-station-event opt]))}
                opt])])]))))
@@ -85,37 +78,41 @@
   [:div.nes-container.with-title.is-centered.mt-3.mx-3
    [:p.title "Search Route"]
    [:button.nes-btn.is-primary
-{:on-click #(rf/dispatch [:download-schedule])}
+    {:on-click #(rf/dispatch [:download-schedule])}
     "Schedule"]
    [:button.nes-btn.is-primary
     {:on-click #(rf/dispatch [:load-all-stops])}
     "Search"]
+   [:button.nes-btn.is-primary
+    {:on-click #(rf/dispatch [:clear-search-selection])}
+    "Clear Selection"]
 
-   [searchable-dropdown :available-stations "First Station" :set-station-1]
-   [searchable-dropdown :available-stations-2 "Second Station" :set-station-2]
+   [searchable-dropdown :available-stations "First Station" :set-station-1 :station-1 :set-query-1 :query-1]
+   [searchable-dropdown :available-stations-2 "Second Station" :set-station-2 :station-2 :set-query-2 :query-2]
    (let [can-submit-search (rf/subscribe [:can-submit-search])]
      [:button.nes-btn.mt-3
-      {:on-click #(rf/dispatch [:format-schedule-stations]) :class (if @can-submit-search "is-primary" "is-disabled")}
+      ;;   {:on-click #(rf/dispatch [:format-schedule-stations]) :class (if @can-submit-search "is-primary" "is-disabled")}
+      {:on-click #(rf/dispatch [:navigate-to-schedule]) :class (if @can-submit-search "is-primary" "is-disabled")}
       "Get Schedule"])])
 
 (defn schedule-page []
   (let [schedule (rf/subscribe [:schedule])
-        stations (rf/subscribe [:schedule-stations])]
-    (r/with-let [active-bound (r/atom "inbound")
+        stations [(:station1 @schedule) (:station2 @schedule)]]
+    (r/with-let [active-bound (r/atom :inbound)
                  active-key   (r/atom nil)]
-      (let [station-1 (-> @stations first :stop_name)
-            station-2 (-> @stations second :stop_name)
+      (let [station-1 (-> stations first :stop_name)
+            station-2 (-> stations second :stop_name)
             _ (when (and (nil? @active-key) (seq @schedule))
                 (reset! active-key (-> @schedule (get @active-bound) keys first)))
             schedule-list (get-in @schedule [@active-bound @active-key])]
         [:<>
          [:div.flex.flex-col.nes-container.p-1
           [:div.flex.flex-row
-           [:button.nes-btn.grow {:class (when (= @active-bound "inbound") "is-primary is-disabled")
-                             :on-click #(reset! active-bound "inbound")}
+           [:button.nes-btn.grow {:class (when (= @active-bound :inbound) "is-primary is-disabled")
+                                  :on-click #(reset! active-bound :inbound)}
             "inbound"]
-           [:button.nes-btn.grow {:class (when (= @active-bound "outbound") "is-primary is-disabled")
-                             :on-click #(reset! active-bound "outbound")}
+           [:button.nes-btn.grow {:class (when (= @active-bound :outbound) "is-primary is-disabled")
+                                  :on-click #(reset! active-bound :outbound)}
             "outbound"]]
           [:div.flex.flex-row
            (for [k (keys (get @schedule @active-bound))]
@@ -127,14 +124,14 @@
          [:table.nes-table.is-bordered
           [:thead
            [:tr
-            [:th (if (= @active-bound "inbound") station-1 station-2)]
-            [:th (if (= @active-bound "inbound") station-2 station-1)]]]
+            [:th (if (= @active-bound :inbound) station-1 station-2)]
+            [:th (if (= @active-bound :inbound) station-2 station-1)]]]
           [:tbody
            (for [i schedule-list]
              ^{:key (str (:time1 i) "-" (:time2 i))}
              [:tr
-              [:td (u/format-time (if (= @active-bound "inbound") (:time1 i) (:time2 i)))]
-              [:td (u/format-time (if (= @active-bound "inbound") (:time2 i) (:time1 i)))]])]]]))))
+              [:td (u/format-time (if (= @active-bound :inbound) (:time1 i) (:time2 i)))]
+              [:td (u/format-time (if (= @active-bound :inbound) (:time2 i) (:time1 i)))]])]]]))))
 
 (defn header []
   [:div.bg-red-500.flex.justify-between
@@ -145,18 +142,44 @@
 (defn root-component []
   [:div.flex.flex-col.w-full.max-w-lg.mx-auto
    [header]
-   [:<>
-    ;; [search-page]
-    [schedule-page]
-    
-    
-    ]])
+   (let [view @(rf/subscribe [:current-view])]
+     (when view
+       [view]))])
+
+(def routes
+  [["/search"
+    {:name :search
+     :view search-page
+     :controllers
+     [{:start (fn [_] (rf/dispatch [:clear-search-selection]))
+       :stop  (fn [_] (js/console.log "Leaving search"))}]}]
+
+   ["/schedule"
+    {:name :schedule
+     :view schedule-page
+     :controllers
+     [{:parameters {:query [:stop1 :stop2]}   ;; make `:id` available
+       :start (fn [{:keys [query]}]
+                (rf/dispatch [:get-schedule [(:stop1 query) (:stop2 query)]]))
+       :stop (fn [_]
+               (rf/dispatch [:clear-schedule]))}]}]])
+
+(def router (reitit/router routes))
+
+(defn init-routes! []
+  (rfe/start!
+   router
+   (fn [match]
+     ;; called whenever URL changes
+     (rf/dispatch [:navigated match]))
+   {:use-fragment true}))
 
 (defonce root (delay (rdomc/create-root (.getElementById js/document "root"))))
 
 (defn ^:export ^:dev/after-load init []
   (rf/dispatch-sync [:init-local-storage])
-  (rf/dispatch-sync [:init-db]) 
+  (rf/dispatch-sync [:init-db])
+  (init-routes!)
   (rdomc/render @root [root-component])
-;;   (rdomc/render @root [query-explorer])
+  ;;   (rdomc/render @root [query-explorer])
   )
--- src/metrajay/events.cljs
+++ src/metrajay/events.cljs
@@ -8,6 +8,8 @@
    [clojure.set :as set]
    [metrajay.env :as env]
    [akiroz.re-frame.storage :refer [persist-db-keys]]
+   [reitit.frontend.controllers :as rfc]
+   [reitit.frontend.easy :as rfe]
    [metrajay.util :as u]))
 
 (rf/reg-fx
@@ -29,16 +31,22 @@
 
 (persisted-reg-event-db :init-local-storage (fn [db] db))
 
+(rf/reg-event-db
+ :navigated
+ (fn [db [_ match]]
+   (let [old-match (:current-route db)
+         controllers (rfc/apply-controllers (:controllers old-match) match)]
+     (assoc db :current-route (assoc match :controllers controllers)))))
+
+
 (rf/reg-event-fx
  :init-db
  (fn [{:keys [db]} [_ _]]
    (let [loaded (exists? (.-duckdbLoaded js/window))]
-     (js/console.log "Checking if DuckDB is loaded..." loaded)
+     (js/console.log "Waiting for duckdb to initialize..." loaded)
      (if (= true loaded)
        {:dispatch [:check-db]}
-       (do
-         (js/setTimeout #(rf/dispatch [:init-db]) 200)
-         {})))))
+       {:dispatch-later {:ms 200 :dispatch [:init-db]}}))))
 
 (rf/reg-event-fx
  :load-db-from-indexeddb
@@ -132,52 +140,46 @@
              :on-success [:set-all-stops]
              :on-failure [:bad-fetch-result]}}))
 
-
-(rf/reg-event-fx
- :format-schedule-stations
- (fn [{:keys [db]} [_ _]]
-   (let [all-stops (:all-stops db)
-         station1-name (:station-1 db)
-         station2-name (:station-2 db)
-         station1-objs (filter #(= station1-name (get % :stop_name)) all-stops)
-         station2-objs (filter #(= station2-name (get % :stop_name)) all-stops)
-         station1-routes (set (mapv #(:route_id %) station1-objs))
-         station2-routes (set (mapv #(:route_id %) station2-objs))
-         routes (set/intersection station1-routes station2-routes)
-         station1 (first (filter #(contains? routes (:route_id %)) station1-objs))
-         station2 (first (filter #(= (:route_id station1) (:route_id %)) station2-objs))
-         result (if (>
-                     (int (:stop_sequence station1))
-                     (int (:stop_sequence station2)))
-                  [station1 station2]
-                  [station2 station1])]
-     {:db (assoc db :schedule-stations result)
-      :dispatch [:get-schedule]})))
-
 (rf/reg-event-fx
  :get-schedule
- (fn [{:keys [db]} [_ _]]
-   (let [station1 (-> db :schedule-stations first :stop_id)
-         station2 (-> db :schedule-stations second :stop_id)
+ (fn [{:keys [db]} [_ [station1 station2]]]
+   (let [loaded (.-dbLoaded js/window)
          query (duck/schedule-query station1 station2)]
-     {:duckdb {:query query
-               :on-success [:format-schedule]
-               :on-failure [:bad-fetch-result]}})))
+     (if loaded
+       {:duckdb {:query query
+                 :on-success [:format-schedule]
+                 :on-failure [:bad-fetch-result]}}
+       {:dispatch-later {:ms 200 :dispatch [:get-schedule [station1 station2]]}}))))
+
+
+(rf/reg-event-fx
+ :init-db
+ (fn [{:keys [db]} [_ _]]
+   (let [loaded (exists? (.-duckdbLoaded js/window))]
+     (if (= true loaded)
+       {:dispatch [:check-db]}
+       {:dispatch-later {:ms 200 :dispatch [:init-db]}}))))
 
 (rf/reg-event-fx
  :bad-fetch-result
  (fn [{:keys [db]} [_ error]]
    {:db (assoc db :error error)}))
 
+(defn get-obj-for-station [station-name, all-stops]
+  (let [first-matching (first (filter #(= station-name (:stop_name %)) all-stops))]
+    (select-keys first-matching [:stop_id :stop_name])))
+
 (rf/reg-event-fx
  :set-station-1
  (fn [{:keys [db]} [_ station-name]]
-   {:db (assoc db :station-1 station-name)}))
+   (let [all-stops (:all-stops db)]
+     {:db (assoc db :station-1 (get-obj-for-station station-name all-stops))})))
 
 (rf/reg-event-fx
  :set-station-2
  (fn [{:keys [db]} [_ station-name]]
-   {:db (assoc db :station-2 station-name)}))
+   (let [all-stops (:all-stops db)]
+     {:db (assoc db :station-2 (get-obj-for-station station-name all-stops))})))
 
 (persisted-reg-event-db
  :update-last-updated
@@ -204,38 +206,69 @@
  (fn [db [_ val]]
    (assoc db :all-stops val)))
 
+(rf/reg-event-db
+ :set-query-1
+ (fn [db [_ val]]
+   (assoc db :query-1 val)))
+
+(rf/reg-event-db
+ :set-query-2
+ (fn [db [_ val]]
+   (assoc db :query-2 val)))
+
+(rf/reg-event-fx
+ :navigate-to-schedule
+ (fn [{:keys [db]} [_ val]]
+   (let [all-stops (:all-stops db)
+         station1-name (-> db :station-1 :stop_name)
+         station2-name (-> db :station-2 :stop_name)
+         station1-objs (filter #(= station1-name (get % :stop_name)) all-stops)
+         station2-objs (filter #(= station2-name (get % :stop_name)) all-stops)
+         station1-routes (set (mapv #(:route_id %) station1-objs))
+         station2-routes (set (mapv #(:route_id %) station2-objs))
+         routes (set/intersection station1-routes station2-routes)
+         station1 (first (filter #(contains? routes (:route_id %)) station1-objs))
+         station2 (first (filter #(= (:route_id station1) (:route_id %)) station2-objs))
+         result (if (>
+                     (int (:stop_sequence station1))
+                     (int (:stop_sequence station2)))
+                  [station1 station2]
+                  [station2 station1])]
+     (rfe/push-state :schedule {}
+                     {:stop1 (-> result first :stop_id)
+                      :stop2 (-> result second :stop_id)}))))
+
 (rf/reg-event-fx
  :set-all-stops
  (fn [{:keys [db]} [_ val]]
-   {:db (assoc db :all-stops val)
-    :dispatch [:get-schedule]}))
+   {:db (assoc db :all-stops val)}))
 
-(defn timekey [day-group] 
-  (let [time-strings (mapv #(str (:time1 %) ";" (:time2 %)) day-group)] 
+(defn timekey [day-group]
+  (let [time-strings (mapv #(str (:time1 %) ";" (:time2 %)) day-group)]
     (str/join ";" time-strings)))
 
 (defn get-day-name [_ group]
   (let [weekday-list  (map #(-> % first :weekday) group)
         sorted (sort-by u/day-order weekday-list)
-        compressed (u/compress-days sorted)] 
+        compressed (u/compress-days sorted)]
     [compressed (first group)]))
 
 (defn format-schedule-group [coll]
   (let [by-day (partition-by :schedule_day coll)
         grouped (group-by timekey by-day)
-        update-keys (u/remap-keys-and-vals grouped get-day-name)] 
+        update-keys (u/remap-keys-and-vals grouped get-day-name)]
     update-keys))
 
 (rf/reg-event-fx
  :format-schedule
  (fn [{:keys [db]} [_ val]]
-   (let [by-direction (group-by :direction_id val)
-         formatted {"inbound" (format-schedule-group (get by-direction "inbound"))
-                    "outbound" (format-schedule-group (get by-direction "outbound"))}]
-     {
-      :dispatch [:set-schedule formatted]
-      
-      })))
+   (let [station (-> val first)
+         by-direction (group-by :direction_id val)
+         formatted {:inbound (format-schedule-group (get by-direction "inbound"))
+                    :outbound (format-schedule-group (get by-direction "outbound"))
+                    :station1 {:stop_name (:stop_name1 station) :stop_id (:stop_id1 station)}
+                    :station2 {:stop_name (:stop_name2 station) :stop_id (:stop_id2 station)}}]
+     {:dispatch [:set-schedule formatted]})))
 
 (rf/reg-event-db
  :set-sample-query
@@ -249,7 +282,10 @@
              :on-success [:set-sample-query]
              :on-failure [:bad-fetch-result]}}))
 
-
+(rf/reg-event-db
+ :clear-search-selection
+ (fn [db [_ _]]
+   (assoc db :station-1 nil :station-2 nil :query-1 "" :query-2 "")))
 
 
 ;; Subscriptions
@@ -260,6 +296,14 @@
    (get db :all-stops [])))
 
 (rf/reg-sub
+ :current-route
+ (fn [db _] (:current-route db)))
+
+(rf/reg-sub
+ :current-view
+ (fn [db _] (-> db :current-route :data :view)))
+
+(rf/reg-sub
  :available-stations
  :<- [:all-stops]
  (fn [all-stops _]
@@ -268,18 +312,17 @@
          sorted (sort distinct-stops)]
      sorted)))
 
-
 (rf/reg-sub
  :available-stations-2
  :<- [:all-stops]
  :<- [:station-1]
  (fn [[all-stops station-1] _]
-   (let [all-obj-for-station (filter #(= (:stop_name %) station-1) all-stops)
+   (let [all-obj-for-station (filter #(= (:stop_id %) (:stop_id station-1)) all-stops)
          all-routes (set (mapv #(:route_id %) all-obj-for-station))
          all-stations-for-route (filter #(contains? all-routes (get % :route_id)) all-stops)
          all-names (mapv #(get % :stop_name) all-stations-for-route)
          distinct-stops (distinct all-names)
-         exclude-self (filter #(not= station-1 %) distinct-stops)
+         exclude-self (filter #(not= (:stop_name station-1) %) distinct-stops)
          sorted (sort exclude-self)]
      sorted)))
 
@@ -292,6 +335,16 @@
  :station-2
  (fn [db _]
    (get db :station-2)))
+
+(rf/reg-sub
+ :query-1
+ (fn [db _]
+   (get db :query-1)))
+
+(rf/reg-sub
+ :query-2
+ (fn [db _]
+   (get db :query-2)))
 
 (rf/reg-sub
  :schedule-stations
